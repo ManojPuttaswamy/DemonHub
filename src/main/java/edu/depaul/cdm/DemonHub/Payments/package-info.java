8a6cbfbd55f99dbaa2db30de459024645a31b5d1
MeghaShree <meghashreebl@MeghaShrees-MBP.attlocal.net>
2024-09-23 14:30:58 -0500

    SE452/added package info

---
+++ src/main/java/edu/depaul/cdm/DemonHub/Payments/package-info.java
@@ -0,0 +1,6 @@
+package edu.depaul.cdm.DemonHub.Payments;
+/************************************************************************************************************
+ * This package handles payment-related functionalities.
+ * 
+ * @author: Meghashree Badri Lakshmi Narasimhan
+ ************************************************************************************************************/
