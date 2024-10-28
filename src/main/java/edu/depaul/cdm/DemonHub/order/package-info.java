8d8576237bde1a583b65b96fb4602809e5337dc1
Meghashree_BL <meghashreebl@gmail.com>
2024-10-28 17:18:42 -0400

    updated package-info.java

---
+++ src/main/java/edu/depaul/cdm/DemonHub/order/package-info.java
@@ -0,0 +1,6 @@
+package edu.depaul.cdm.demonhub.order;
+/************************************************************************************************************
+ * This package handles Order-related functionalities.
+ * 
+ * @author: Meghashree Badri Lakshminarasimhan
+ ************************************************************************************************************/
