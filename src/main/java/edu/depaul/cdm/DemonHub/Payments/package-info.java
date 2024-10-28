1370715c63b6b3ca823c3d1b965d63ef7fc70f82
serhat111 <49848798+serhat111@users.noreply.github.com>
2024-10-28 16:08:26 -0500

    minor fixes

--- src/main/java/edu/depaul/cdm/DemonHub/Payments/package-info.java
+++
@@ -1,6 +0,0 @@
-package edu.depaul.cdm.DemonHub.Payments;
-/************************************************************************************************************
- * This package handles payment-related functionalities.
- * 
- * @author: Meghashree Badri Lakshmi Narasimhan
- ************************************************************************************************************/
